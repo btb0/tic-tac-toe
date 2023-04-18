ac431616ff46b7e997863407dcb981983a626f14
btb0 <benburke2016@gmail.com>
2023-04-17 22:23:15 -0400

    mvp

--- js/script.js
+++ js/script.js
@@ -1,40 +1,114 @@
 /*----- constants -----*/
-const COLORS = {
-    '0': null,
+const PLAYERS = {
     '1': 'X',
     '-1': 'O'
-}
-
-/*----- state variables -----*/
-let board 
-let turn
-let winner
-
-/*----- cached elements  -----*/
-const playAgainBtn = document.querySelector('button')
-
-/*----- event listeners -----*/
-playAgainBtn.addEventListener('click', restartGame)
-
-/*----- functions -----*/
-init()
-
-function init() {
-    // rotate 90 degrees counter clockwise to visualize the boards mapping to the DOM
-    board = [ 
-        [0, 0, 0],  // t7, t4, t1
-        [0, 0, 0],  // t8, t5, t2
-        [0, 0, 0], // t9, t6, t3
-    ]
+  }
+  
+  const winningCombos = [
+    // Horizontal 
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    // Vertical
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    // Diagonal
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
+  
+  /*----- state variables -----*/
+  let board
+  let turn
+  let winner
+  let tie
+  
+  /*----- cached elements  -----*/
+  const playAgainBtn = document.getElementById('restart')
+  const boardEl = document.getElementById('board')
+  const msgEl = document.getElementById('message')
+  
+  /*----- event listeners -----*/
+  playAgainBtn.addEventListener('click', restartGame)
+  boardEl.addEventListener('click', handleClick)
+  
+  /*----- functions -----*/
+  init()
+  
+  function init() {
     turn = 1   // X's turn first
+    board = [null, null, null, null, null, null, null, null, null]
     winner = null
+    tie = false
+    playAgainBtn.style.visibility = 'hidden';
     render()
-}
-
-function render() {
-    
-}
-
-function restartGame() {
+  }
+  
+  function render() {
+    renderBoard()
+    renderMsg()
+    renderCtrls()
+  }
+  
+  function restartGame() {
     init()
-}
+  }
+  
+  function handleClick(evt) {
+    if (winner) return
+    console.log(board)
+    if (evt.target.id !== 'board') {
+      const tIdx = evt.target.id[1]
+      if (!board[tIdx]) {
+        board[tIdx] = turn
+        checkWin()
+        changeTurn()
+        render()
+      }
+    }
+  }
+  
+  function renderBoard() {
+    board.forEach(function(tile, idx) {
+      if (tile) {
+        document.getElementById(`t${idx}`).textContent = PLAYERS[tile]
+      } else {
+        document.getElementById(`t${idx}`).textContent = ''
+      }
+    })
+  }
+  
+  function renderCtrls() {
+    if ((winner !== null) || (tie === true)) {
+     playAgainBtn.style.visibility = 'visible';
+    }
+  }
+  
+  function renderMsg() {
+    if (!board.includes(null)) {
+      tie = true
+    }
+    if (winner) {
+      msgEl.innerText = `${PLAYERS[winner]} Wins!`
+    } else if (!winner && tie) {
+      msgEl.innerText = 'It\'s a tie!'
+    } else {
+      msgEl.innerText = `${PLAYERS[turn]}'s Turn`
+    }
+  }
+  
+  function checkWin() {
+    for (let combo of winningCombos) {        // for each combo(subarray/ 2d array) of winningCombos array
+      let playerWin = 0 
+      combo.forEach(c => {                  // for each item in the combo(subaray / 2d array)
+        playerWin += board[c]              // add a value of that item(1 or -1 / X or O) to playerWin
+        if (Math.abs(playerWin) === 3) winner = turn     // if playerWins absolute value === 3 the winner is    
+      })                                                // whoever's turn it was
+    }
+  }
+  
+  function changeTurn() {
+    turn *= -1
+  }
+  
